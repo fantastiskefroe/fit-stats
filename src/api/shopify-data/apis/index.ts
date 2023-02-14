6100fff6e46ceb4891cbf7c527de833d1c7548d4
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2023-02-14 21:29:23 +0100

    Add api

---
+++ src/api/shopify-data/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './OrderControllerApi';
+export * from './ProductControllerApi';
+export * from './StatsControllerApi';
+export * from './WebhookControllerApi';
